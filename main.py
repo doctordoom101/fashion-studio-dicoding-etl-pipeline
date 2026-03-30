71b043748ca16ddb8cc9b6c025acc8764c4a336c
Defanda Yeremia <144663115+doctordoom101@users.noreply.github.com>
2026-03-30 22:00:06 +0700

    chore: fix load data script

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 ﻿from utils.extract import scrape_data
 from utils.transform import clean_data
-from utils.load import save_to_csv
+from utils.load import load_all
 
 def main():
     print("Starting ETL Process...")
     df_raw = scrape_data()
     df_clean = clean_data(df_raw)
-    save_to_csv(df_clean)
+    load_all(df_clean)
     print("ETL Process Completed.")
 
 if __name__ == "__main__":
